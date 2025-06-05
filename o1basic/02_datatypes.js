b518234702dd2dfec5117206722dc0a959eeb323
nirmallo1c <rradiant.nirmal@gmail.com>
2025-06-05 14:11:25 +0000

    changed file name

---
+++ o1basic/02_datatypes.js
@@ -0,0 +1,18 @@
+
+let name="nirmal"
+let age=28
+let isLoggdIn=false
+
+
+//alert(3+3);
+// string
+// bigInt
+// boolean=>true/false
+// null
+// undefined
+// symbol
+
+// object
+
+
+console.log(typeof "name");
